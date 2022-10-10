3ddcafda7671775fd38eb2f4d2c4bd9ec3169c31
xxia00 <112386172+xxia00@users.noreply.github.com>
2022-10-10 22:21:21 +0800

    Add files via upload
    vega

---
+++ vega_lite_vis.js
@@ -0,0 +1,33 @@
+var vg_1 = "map_happiness.vg.json";
+vegaEmbed("#map_happiness", vg_1,{"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+
+var vg_1 = "rank.json";
+vegaEmbed("#rank", vg_1,{"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_1 = "radial2.json";
+vegaEmbed("#scatter", vg_1,{"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+
+
+var vg_1 = "radial1.json";
+vegaEmbed("#scatter3", vg_1,{"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_1 = "scatterplot.json";
+vegaEmbed("#scatterplot", vg_1,{"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_1 = "heatmap.json";
+vegaEmbed("#heatmap", vg_1,{"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
